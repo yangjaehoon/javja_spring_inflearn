9e126c4e088600f7c25d9bc15e94edbd77e59cfe
Jaehoon Yang <lljhms@naver.com>
2025-01-17 00:31:04 +0900

    model을 넘겨주는 컨트롤러

--- servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v3/controller/MemberFormControllerV3.java
@@ -8,6 +8,7 @@ import java.util.Map;
 public class MemberFormControllerV3 implements ControllerV3 {
     @Override
     public ModelView process(Map<String, String> paramMap) {
+
         return new ModelView("new-form");
     }
 }
