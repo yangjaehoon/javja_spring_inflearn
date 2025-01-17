7622d34d44381af025a798d30cd41c35655f6af9
Jaehoon Yang <lljhms@naver.com>
2025-01-17 23:33:49 +0900

    welcome page

--- servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
+++ servlet/src/main/java/hello/servlet/web/springmvc/v3/SpringMemberControllerV3.java
@@ -1,2 +1,46 @@
-package hello.servlet.web.springmvc.v3;public class SpringMemberControllerV3 {
+package hello.servlet.web.springmvc.v3;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/springmvc/v3/members")
+public class SpringMemberControllerV3 {
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @GetMapping("/new-form")
+    //@RequestMapping(value = "/new-form", method = RequestMethod.GET)
+    public String newForm(){
+        return "new-form";
+    }
+
+    @PostMapping("/save")
+    //@RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(
+            @RequestParam("username") String username,
+            @RequestParam("age") int age,
+            Model model)
+    {
+
+        Member member = new Member(username, age);
+        memberRepository.save(member);
+
+        model.addAttribute("member", member);
+        return "save-result";
+
+    }
+
+    @GetMapping
+    //@RequestMapping(method = RequestMethod.GET)
+    public String membes(Model model) {
+        List<Member> members = memberRepository.findAll();
+
+        model.addAttribute("members", members);
+        return "members";
+    }
 }
