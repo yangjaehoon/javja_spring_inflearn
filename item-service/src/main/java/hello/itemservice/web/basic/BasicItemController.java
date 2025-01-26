63b68a217e0fc59d18d0998d388ca6361d26c3b4
Jaehoon Yang <lljhms@naver.com>
2025-01-27 00:04:03 +0900

    상품 상세

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -24,10 +25,17 @@ public class BasicItemController {
 //    } @RequiredArgsContructor가 final이 붙은 필드의 생성자를 자동으로 생성해줌
 
     @GetMapping
-    public String item(Model model){
+    public String items(Model model){
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items);
         return "basic/items";
+    }
+
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable long itemId, Model model){
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 
     @PostConstruct
