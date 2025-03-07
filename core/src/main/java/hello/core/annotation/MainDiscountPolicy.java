151b915b8172a8fd59e93ed201e6cce1d588dd1c
Jaehoon Yang <lljhms@naver.com>
2025-03-07 18:40:59 +0900

    임시

--- core/src/main/java/hello/core/annotation/MainDiscountPolicy.java
+++ core/src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -1,13 +1,14 @@
 package hello.core.annotation;
 
-//import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Qualifier;
+
 import java.lang.annotation.*;
 
 @Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @Documented
-//@Qualifier("mainDiscountPolicy")
+@Qualifier("mainDiscountPolicy")
 public @interface MainDiscountPolicy {
 
 }
