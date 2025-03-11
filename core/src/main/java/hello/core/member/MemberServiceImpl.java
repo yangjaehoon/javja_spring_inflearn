b5499d57840552c91227535f10d52474feda44d5
Jaehoon Yang <lljhms@naver.com>
2025-03-11 15:12:52 +0900

    변경사항

--- core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -7,10 +7,12 @@ import org.springframework.stereotype.Component;
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository;
+
     @Autowired
     public MemberServiceImpl(MemberRepository memberRepository){
         this.memberRepository = memberRepository;
     }
+
     @Override
     public void join(Member member) {
         memberRepository.save(member);
