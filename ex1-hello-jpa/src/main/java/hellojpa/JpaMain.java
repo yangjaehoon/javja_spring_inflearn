6fd8dd2bf4a47a52f438fad4113971c7e58a3ef1
Jaehoon Yang <lljhms@naver.com>
2025-04-10 17:57:32 +0900

    Hello JPA - 애플리케이션 개발

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -2,6 +2,7 @@ package hellojpa;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class JpaMain {
@@ -10,11 +11,36 @@ public class JpaMain {
 
         EntityManager em = emf.createEntityManager();
 
-        Member member = new Member();
-        em.persist(member);
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try{
+            //등록
+//            Member member = new Member();
+//            member.setId(2L);
+//            member.setName("HelloB");
+
+//            //조회
+//            Member findMember = em.find(Member.class, 1L);
+//            System.out.println("findMember.id = " + findMember.getId());
+//            System.out.println("findMember.name = " + findMember.getName());
+
+            //삭제
+            //em.remove(findMember);
+
+            Member findMember = em.find(Member.class, 1);
+            findMember.setName("HelloJPA");
 
 
-        em.close();
+
+            //em.persist(member);
+            tx.commit();
+        }
+        catch (Exception e){
+            tx.rollback();
+        }finally {
+            em.close();
+        }
 
         emf.close();
     }
